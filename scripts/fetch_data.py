3a8a8646e3b1970572b5969b93586bce107fc3cc
c4ffe14e <c4ffe14e@noreply.codeberg.org>
2025-06-30 21:29:56 +0200

    use a context manager in fetch script

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -33,37 +33,37 @@ urls: list[str] = [
     "https://www.facebook.com/watch/search/?q=python",
 ]
 
-client: httpx.Client = httpx.Client(
-    headers={
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Sec-Fetch-Mode": "navigate",
-    }
-)
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Sec-Fetch-Mode": "navigate",
+}
+
 
 doc_ids: dict[str, int] = {}
 extra_vars: dict[str, int] = {}
-for i in urls:
-    r: httpx.Response = client.get(i)
-    scripts_urls: list[str] = re.findall(r'<link rel="preload" href="(.*)" as="script"', r.text)
+with httpx.Client(headers=headers) as client:
+    for i in urls:
+        r: httpx.Response = client.get(i)
+        scripts_urls: list[str] = re.findall(r'<link rel="preload" href="(.*)" as="script"', r.text)
 
-    for j in scripts_urls:
-        script: str = client.get(j).text
+        for j in scripts_urls:
+            script: str = client.get(j).text
 
-        for name in api_names[:]:
-            ids = re.search(rf'^__d\("{name}_facebookRelayOperation",.*exports="([0-9]+)"', script, re.MULTILINE)
-            if ids:
-                doc_ids[name] = int(ids.group(1))
-                api_names.remove(name)
+            for name in api_names[:]:
+                ids = re.search(rf'^__d\("{name}_facebookRelayOperation",.*exports="([0-9]+)"', script, re.MULTILINE)
+                if ids:
+                    doc_ids[name] = int(ids.group(1))
+                    api_names.remove(name)
 
-        ev: list[str] = re.findall(
-            r'name:"(__relay_internal__pv__[^"]+provider)"',
-            script,
-            re.MULTILINE,
-        )
-        for v in ev:
-            extra_vars[v] = 0
+            ev: list[str] = re.findall(
+                r'name:"(__relay_internal__pv__[^"]+provider)"',
+                script,
+                re.MULTILINE,
+            )
+            for v in ev:
+                extra_vars[v] = 0
 
 if api_names:
     print("Failed to fetch ids for:", *api_names, file=sys.stderr)
