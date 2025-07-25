5be02c0e91ab29a681986fd5d3c59e935d95353d
caffeine <>
2025-07-25 14:41:48 +0200

    refactor fetch_data.py

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import httpx
 import orjson
 
-api_names: list[str] = [
+QUERY_NAMES: list[str] = [
     "CometAlbumPhotoCollagePaginationQuery",
     "CometGroupDiscussionRootSuccessQuery",
     "CometGroupRootQuery",
@@ -24,53 +24,70 @@ api_names: list[str] = [
     "ProfilePlusCometLoggedOutRootQuery",
     "SearchCometResultsPaginatedResultsQuery",
 ]
-
-urls: list[str] = [
+URLS: list[str] = [
     "https://www.facebook.com/facebook",
     "https://www.facebook.com/facebook/posts/1193563419482343",
     "https://www.facebook.com/groups/python/",
     "https://www.facebook.com/media/set/?set=a.10152716010956729",
     "https://www.facebook.com/watch/search/?q=python",
 ]
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0",
+HEADERS: dict[str, str] = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:141.0) Gecko/20100101 Firefox/141.0",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.5",
+    "Alt-Used": "www.facebook.com",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
     "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "same-origin",
+    "TE": "trailers",
 }
-
-
-doc_ids: dict[str, int] = {}
-extra_vars: dict[str, int] = {}
-with httpx.Client(headers=headers) as client:
-    for i in urls:
-        r: httpx.Response = client.get(i)
-        scripts_urls: list[str] = re.findall(r'<link rel="preload" href="(.*)" as="script"', r.text)
-
-        for j in scripts_urls:
-            script: str = client.get(j).text
-
-            for name in api_names.copy():
-                ids = re.search(rf'^__d\("{name}_facebookRelayOperation",.*exports="([0-9]+)"', script, re.MULTILINE)
-                if ids:
-                    doc_ids[name] = int(ids.group(1))
-                    api_names.remove(name)
-
-            ev: list[str] = re.findall(
-                r'name:"(__relay_internal__pv__[^"]+provider)"',
-                script,
-                re.MULTILINE,
-            )
-            for v in ev:
-                extra_vars[v] = 0
-
-if api_names:
-    print("Failed to fetch ids for:", *api_names, file=sys.stderr)
-
 DATA_PATH: Path = Path("src") / "lib" / "data"
-with (DATA_PATH / "doc_ids.json").open("w") as f:
-    f.write(orjson.dumps(doc_ids, option=orjson.OPT_INDENT_2).decode())
 
-with (DATA_PATH / "extra_variables.json").open("w") as f:
-    f.write(orjson.dumps(extra_vars, option=orjson.OPT_INDENT_2).decode())
+
+def main() -> int:
+    doc_ids: dict[str, int] = {}
+    extra_vars: dict[str, int] = {}
+
+    with httpx.Client(headers=HEADERS) as client:
+        for url in URLS:
+            html_res: httpx.Response = client.get(url)
+            scripts_urls: list[str] = re.findall(r'<link rel="preload" href="(.*)" as="script"', html_res.text)
+
+            for script in scripts_urls:
+                script_res: httpx.Response = client.get(script)
+                script_cont: str = script_res.text
+
+                for name in QUERY_NAMES.copy():
+                    id_match: re.Match[str] | None = re.search(
+                        rf'^__d\("{name}_facebookRelayOperation",.*exports="([0-9]+)"',
+                        script_cont,
+                        re.MULTILINE,
+                    )
+                    if id_match:
+                        doc_ids[name] = int(id_match.group(1))
+                        QUERY_NAMES.remove(name)
+
+                extra_variables_matches: list[str] = re.findall(
+                    r'name:"(__relay_internal__pv__[^"]+provider)"',
+                    script_cont,
+                    re.MULTILINE,
+                )
+                for var in extra_variables_matches:
+                    extra_vars[var] = 0
+
+    if QUERY_NAMES:
+        print("Failed to fetch ids for:", *QUERY_NAMES, file=sys.stderr)
+        return 1
+
+    with (DATA_PATH / "doc_ids.json").open("w") as f:
+        f.write(orjson.dumps(doc_ids, option=orjson.OPT_INDENT_2).decode())
+
+    with (DATA_PATH / "extra_variables.json").open("w") as f:
+        f.write(orjson.dumps(extra_vars, option=orjson.OPT_INDENT_2).decode())
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
