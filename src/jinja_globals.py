bcab57f0b0a3b557aa0b35684379e9f8986e97e0
caffeine <>
2025-08-09 18:41:50 +0200

    refactor theme loading

--- src/jinja_globals.py
+++ src/jinja_globals.py
@@ -1,4 +1,3 @@
-from collections.abc import Callable
 from typing import Any
 
 from .flask_utils import get_config, get_user_settings
@@ -8,8 +7,17 @@ def types(obj: object) -> str:
     return type(obj).__name__
 
 
-GLOBALS: dict[str, Callable[..., Any]] = {
+THEMES: dict[str, str] = {
+    "default": "Default",
+    "light": "Light",
+    "black": "Black",
+    "catppuccin_mocha": "Catppuccin mocha",
+    "catppuccin_latte": "Catppuccin latte",
+}
+
+GLOBALS: dict[str, Any] = {
     "type": types,
     "get_config": get_config,
     "get_user_settings": get_user_settings,
+    "THEMES": THEMES,
 }
